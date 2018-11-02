796b1a6946ac9ffdf2327ff7c904af1275b63083
zuber <zuber1077@yahoo.com>
2018-11-02 17:28:12 +0800

    delete posts

--- models/user.js
+++ models/user.js
@@ -4,15 +4,8 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
 	email: String,
-	image: String,
-	posts: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Post'
-		}
-	]
+	image: String
 });
-
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', UserSchema);
